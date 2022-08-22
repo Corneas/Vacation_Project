495220af5e408298c5ea4b944a2027c08673fa59
냠냠 <lanilani7661@gmail.com>
2022-08-22 13:51:26 +0900

    오브젝트 풀링, 보스패턴 추가

--- SummerVacationProject/Assets/Scripts/Test1/Boss.cs
+++ SummerVacationProject/Assets/Scripts/Test1/Boss.cs
@@ -6,10 +6,20 @@ public class Boss : MonoBehaviour
 {
     [SerializeField]
     private GameObject bulletPre;
+    [SerializeField]
+    private GameObject target;
+
+    private List<GameObject> bulletList = new List<GameObject>();
 
     // Dialogue 기능 추가 예정
     private void Start() 
     {
+        for(int i = 0; i < 100; ++i)
+        {
+            var bulletObj = Instantiate(bulletPre, transform.position, Quaternion.identity);
+            bulletObj.SetActive(false);
+            bulletObj.transform.SetParent(PoolManager.Instance.transform);
+        }
         StartCoroutine(BossPattern());
     }
 
@@ -19,6 +29,10 @@ public class Boss : MonoBehaviour
 
         //Pattern 1
         StartCoroutine(CircleFire());
+
+        yield return new WaitForSeconds(10f);
+
+        StartCoroutine(CircleFireGoto());
 
         yield return null;
     }
@@ -37,12 +51,82 @@ public class Boss : MonoBehaviour
             {
                 angle = 20f;
             }
-            for(float j = angle; j < 360; j += angle)
+            for(float j = angle; j <= 360; j += angle)
             {
-                GameObject bullet = Instantiate(bulletPre, transform.position, Quaternion.Euler(0, 0, j));
-                bullet.transform.SetParent(null);
+                GameObject bullet = null;
+
+                bullet = InstaniateOrSpawn(bullet);
+
+                bullet.transform.rotation = Quaternion.Euler(0, 0, j);
+
                 yield return new WaitForSeconds(0.01f);
             }
         }
+    }
+
+    IEnumerator CircleFireGoto()
+    {
+        List<BulletMove> bulletMoves = new List<BulletMove>();
+
+        for(int i = 0; i < 10; ++i)
+        {
+            for (int j = 0; j <= 360; j += 20)
+            {
+                GameObject bullet = null;
+                bullet = InstaniateOrSpawn(bullet);
+                bullet.transform.rotation = Quaternion.Euler(0, 0, j);
+                bulletMoves.Add(bullet.GetComponent<BulletMove>());
+            }
+
+            StartCoroutine(moveToPlayer(bulletMoves.ToArray()));
+
+            yield return new WaitForSeconds(1f);
+            bulletMoves.Clear();
+
+        }
+    }
+
+    IEnumerator moveToPlayer(BulletMove[] bulletMoves)
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        for(int i = 0; i < bulletMoves.Length; ++i)
+        {
+            bulletMoves[i].bulletSpd = 0f;
+        }
+
+        yield return new WaitForSeconds(0.35f);
+
+        for(int i = 0; i < bulletMoves.Length; ++i)
+        {
+            bulletMoves[i].bulletSpd = 10f;
+        }
+
+        for(int i = 0; i < bulletMoves.Length; ++i)
+        {
+            var rot = target.transform.position - bulletMoves[i].transform.position;
+
+            var angle = Mathf.Atan2(rot.y, rot.x) * Mathf.Rad2Deg;
+
+            bulletMoves[i].transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+        yield return null;
+    }
+
+    GameObject InstaniateOrSpawn(GameObject bullet)
+    {
+        if (PoolManager.Instance.transform.childCount > 0)
+        {
+            bullet = PoolManager.Instance.transform.GetChild(0).gameObject;
+            bullet.SetActive(true);
+        }
+        else if (PoolManager.Instance.transform.childCount <= 0)
+        {
+            bullet = Instantiate(bulletPre, transform.position, Quaternion.identity);
+        }
+        bullet.transform.position = transform.position;
+        bullet.transform.SetParent(null);
+
+        return bullet;
     }
 }
--- SummerVacationProject/Assets/Scripts/Test1/BulletMove.cs
+++ SummerVacationProject/Assets/Scripts/Test1/BulletMove.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class BulletMove : MonoBehaviour
 {
-    [SerializeField]
-    private float bulletSpd = 10f;
+    public float bulletSpd = 10f;
+
+    private void Start()
+    {
+        Invoke("Pool", 2f);
+    }
 
     private void Update()
     {
         transform.Translate(Vector3.right * bulletSpd * Time.deltaTime, Space.Self);
         //transform.position += Vector3.right * bulletSpd * Time.deltaTime;
+    }
+
+    public void Pool()
+    {
+        gameObject.SetActive(false);
+        transform.SetParent(PoolManager.Instance.transform);
     }
 
 }
