26aa9b58595c256cc66c6867b939bd82ff8aa80e
냠냠 <lanilani7661@gmail.com>
2022-08-30 18:32:51 +0900

    패턴1 총알 각도를 삼각함수로 대체

--- SummerVacationProject/Assets/Scripts/Test1/Boss.cs
+++ SummerVacationProject/Assets/Scripts/Test1/Boss.cs
@@ -58,28 +58,46 @@ public class Boss : MonoBehaviour
 
     IEnumerator CircleFire()
     {
-        float angle = 0f;
+        float fireAngle = 0f;
+        float angle = 13f;
 
         for(int i = 0; i < 15; ++i)
         {
-            if(i % 2 == 0)
-            {
-                angle = 10f;
-            }
-            else
-            {
-                angle = 20f;
-            }
-            for(float j = angle; j <= 360; j += angle)
+            for(int j = 0; j < 40; ++j)
             {
                 GameObject bullet = null;
 
-                bullet = InstaniateOrSpawn(bullet,gameObject.transform);
+                bullet = InstaniateOrSpawn(bullet, gameObject.transform);
 
-                bullet.transform.rotation = Quaternion.Euler(0, 0, j);
+                Vector2 direction = new Vector2(Mathf.Cos(fireAngle * Mathf.Deg2Rad), Mathf.Sin(fireAngle * Mathf.Deg2Rad));
+                bullet.transform.right = direction;
+
+                fireAngle += angle;
+                if(fireAngle >= 360)
+                {
+                    fireAngle -= 360;
+                }
 
                 yield return new WaitForSeconds(0.01f);
             }
+            //if(i % 2 == 0)
+            //{
+            //    angle = 10f;
+            //}
+            //else
+            //{
+            //    angle = 20f;
+            //}
+            //for(float j = angle; j <= 360; j += angle)
+            //{
+            //    GameObject bullet = null;
+
+            //    bullet = InstaniateOrSpawn(bullet,gameObject.transform);
+
+            //    bullet.transform.rotation = Quaternion.Euler(0, 0, j);
+
+            //    yield return new WaitForSeconds(0.01f);
+            //}
         }
     } // pattern 1
 
