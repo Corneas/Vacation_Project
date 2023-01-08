1e9ff1dfdf0b21013b0da8d67c1e3664e570055e
Minkyu Kwak <lanilani7661@gmail.com>
2023-01-09 00:26:24 +0900

    RopeTest bug fixed

--- SummerVacationProject/Assets/Rope/Scripts/Rope/RopeCreate.cs
+++ SummerVacationProject/Assets/Rope/Scripts/Rope/RopeCreate.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RopeScript : MonoBehaviour
+public class RopeCreate : MonoBehaviour
 {
     [SerializeField]
-    private GameObject ropePrefab;
+    private GameObject ropePre;
     [SerializeField]
     private int ropeCnt;
     [SerializeField]
     private Rigidbody2D pointRig;
 
-    private FixedJoint2D exJoint;
+    FixedJoint2D exJoint;
 
     private void Start()
     {
         for(int i = 0; i < ropeCnt; ++i)
         {
-            FixedJoint2D currentJoint = Instantiate(ropePrefab, transform).GetComponent<FixedJoint2D>();
+            FixedJoint2D currentJoint = Instantiate(ropePre, transform).GetComponent<FixedJoint2D>();
             currentJoint.transform.position = new Vector3(0, (i + 1) * -0.25f, 0);
             if(i == 0)
             {
@@ -37,5 +37,6 @@ public class RopeScript : MonoBehaviour
             }
         }
     }
+
 
 }
