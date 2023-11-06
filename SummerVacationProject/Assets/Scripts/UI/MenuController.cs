d5d3f8b676f96cb31efb046481db8b2dc65682d0
Corneas <lanilani7661@gmail.com>
2023-11-06 16:11:15 +0900

    ui툴킷 이용한 타이틀씬 ui 제작

--- SummerVacationProject/Assets/Scripts/UI/MenuController.cs
+++ SummerVacationProject/Assets/Scripts/UI/MenuController.cs
@@ -1,13 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UIElements;
 
 public class MenuController : MonoBehaviour
 {
-    private UIDocument uiDocument;
+    [Header("Menu")]
+    private UIDocument uiDocument = null;
+    private Button playButton = null;
+    private Button settingButton = null;
+    private Button exitButton = null;
+
+    [SerializeField]
+    private VisualTreeAsset settingsButtonsTemplate = null;
+    private VisualElement settingsButtons = null;
+
+    private VisualElement buttonsWrapper;
 
     private void Awake()
     {
         uiDocument = GetComponent<UIDocument>();
+        playButton = uiDocument.rootVisualElement.Q<Button>("PlayButton");
+        settingButton = uiDocument.rootVisualElement.Q<Button>("SettingButton");
+        exitButton = uiDocument.rootVisualElement.Q<Button>("ExitButton");
+
+        buttonsWrapper = uiDocument.rootVisualElement.Q<VisualElement>("Buttons");
+
+        playButton.clicked += PlayButtonOnClicked;
+        settingButton.clicked += SettingsButtonOnClicked;
+
+        if(settingsButtonsTemplate != null)
+            settingsButtons = settingsButtonsTemplate.CloneTree();
+        else
+            Debug.Log($"settingsButtonsTemplate is null!");      
+
+        var backButton = settingsButtons.Q<Button>("BackButton");
+        backButton.clicked += BackButtonOnClicked;
+        
+    }
+
+    private void PlayButtonOnClicked()
+    {
+        Debug.Log("Play");
+    }
+
+    private void SettingsButtonOnClicked()
+    {
+        buttonsWrapper.Clear();
+        buttonsWrapper.Add(settingsButtons);
+    }
+
+    private void BackButtonOnClicked()
+    {
+        buttonsWrapper.Clear();
+        buttonsWrapper.Add(playButton);
+        buttonsWrapper.Add(settingButton);
+        buttonsWrapper.Add(exitButton);
     }
 }
